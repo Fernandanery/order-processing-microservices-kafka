991a2f6f42dbe1671034985c6617d355cd5f476f
Fernandanery <fernanda.a.nery@hotmail.com>
2025-03-07 11:18:22 -0300

    refactor: Alteração no nome da classe e configurações

---
+++ Delivery.API/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.OpenApi.Models;
+using Delivery.Domain.Services;
+using Delivery.Domain.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IOrderDeliveryService, OrderDeliveryService>();
+builder.Services.AddScoped<IOrderDeliveryRepository, OrderDeliveryRepository>(); // Se existir uma implementação
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Order Delivery API", Version = "v1" });
+
+    var xmlFilename = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath);
+    }
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order Delivery API V1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    if (endpoints != null)
+        endpoints.MapControllers();
+    else
+        throw new ArgumentNullException(nameof(endpoints));
+});
+
+app.Run();
---
+++ Delivery.Domain/Infrastructure/Repositories/IOrderDeliveryRepository.cs
@@ -0,0 +1,40 @@
+﻿using Delivery.Domain.Entities;
+
+namespace Delivery.Domain.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Order Delivery repository
+    /// </summary>
+    public interface IOrderDeliveryRepository
+    {
+        /// <summary>
+        /// Add an order into database
+        /// </summary>
+        /// <param name="order">Order's data</param>
+        void Add(Entities.OrderDelivery order);
+
+        /// <summary>
+        /// Update an order to database
+        /// </summary>
+        /// <param name="order">Order's data</param>
+        void Update(Entities.OrderDelivery order);
+
+        /// <summary>
+        /// Get all orders from database
+        /// </summary>
+        /// <returns>All orders</returns>
+        IList<Entities.OrderDelivery> GetAll();
+
+        /// <summary>
+        /// Get an order from database by its id
+        /// </summary>
+        /// <returns>Order related to the id requested</returns>
+        Entities.OrderDelivery GetOrderById(string OrderId);
+
+        /// <summary>
+        /// Get all orders from database for determined situation
+        /// </summary>
+        /// <returns>All orders those have the requested situation</returns>
+        IList<Entities.OrderDelivery> GetOrdersBySituation(OrderDeliverySituation orderSituation);
+    }
+}
